a0bdc5efc5d4007335e2133d89e5dea3925b4d8d
MARCOS SOUZA <marcos.souza394@etec.sp.gov.br>
2025-06-08 19:21:17 -0300

    Centraliza injeção de dependências do Program.cs

--- ClimaTempo/ClimaTempo.API/Program.cs
+++ ClimaTempo/ClimaTempo.API/Program.cs
@@ -1,23 +1,19 @@
 using ClimaTempo.API.Data.Data;
 using ClimaTempo.API.Domain.Interfaces;
 using ClimaTempo.API.Repositories;
-using Microsoft.EntityFrameworkCore;
+using ClimaTempo.API.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<IClimaRepository, WeatherApiRepository>();
-builder.Services.AddScoped<ICidadeFavoritaRepository, CidadeFavoritaRepository>();
-builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddProjectServices();
+builder.Services.AddMediatRServices();
+builder.Services.AddDatabase(builder.Configuration);
 
 var app = builder.Build();
 
@@ -29,9 +25,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
